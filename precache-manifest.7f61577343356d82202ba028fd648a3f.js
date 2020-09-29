3a9079f785fa31d4b3586a9c76b636ec46c1ca8c
AnisLcool <42942454+AnisLcool@users.noreply.github.com>
2020-09-29 11:29:21 +0100

    Updates

---
+++ precache-manifest.7f61577343356d82202ba028fd648a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1667628487d394e61886144f2f7ff839",
+    "url": "/Pomodoro-react-app/index.html"
+  },
+  {
+    "revision": "5c503da419c31ca548bb",
+    "url": "/Pomodoro-react-app/static/css/main.6a34c0a4.chunk.css"
+  },
+  {
+    "revision": "6f58b6635644f7e5754c",
+    "url": "/Pomodoro-react-app/static/js/2.69929f89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-react-app/static/js/2.69929f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c503da419c31ca548bb",
+    "url": "/Pomodoro-react-app/static/js/main.2c378108.chunk.js"
+  },
+  {
+    "revision": "911ec29161447f8b1fed",
+    "url": "/Pomodoro-react-app/static/js/runtime-main.3812fc9d.js"
+  }
+]);
